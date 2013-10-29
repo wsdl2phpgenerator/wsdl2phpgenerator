6f6cee10cd93599bc049f3caa655d84030f81dd9
dypa <dypa@bk.ru>
2013-10-29 17:38:27 +0400

    [+] correct way to use composer autoloader [!] fix paths to tests (see gitignore)

--- generate.php
+++ generate.php
@@ -9,10 +9,7 @@ use Wsdl2PhpGenerator\Generator;
 use Wsdl2PhpGenerator\Config;
 use \Exception;
 
-require __DIR__ . '/vendor/symfony/class-loader/Symfony/Component/ClassLoader/UniversalClassLoader.php';
-$classloader = new UniversalClassLoader();
-$classloader->registerNamespace('Wsdl2PhpGenerator', array('src', 'lib'));
-$classloader->register();
+require 'vendor/autoload.php';
 
 // Start
 $cli = new Cli('wsdl2php', '[OPTIONS] -i wsdlfile -o directory', '2.0.0');
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,6 +4,4 @@ use Symfony\Component\ClassLoader\UniversalClassLoader;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$classloader = new UniversalClassLoader();
-$classloader->registerNamespace('Wsdl2PhpGenerator', array('src', 'lib', 'tests'));
-$classloader->register();
+require_once __DIR__ . '/Wsdl2PhpGenerator/Tests/Functional/Wsdl2PhpGeneratorFunctionalTestCase.php';
