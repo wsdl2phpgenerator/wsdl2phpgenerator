4aed9af55fe5144951b0a4e09494fa4ba8c6ea10
Kasper Garnaes <kasper.garnaes@gmail.com>
2013-12-03 23:46:01 +0100

    Use relative path when including Composer autoloader.

--- wsdl2php.php
+++ wsdl2php.php
@@ -6,7 +6,7 @@ use Wsdl2PhpGenerator\Console\Application;
 use Wsdl2PhpGenerator\Console\GenerateCommand;
 use Wsdl2PhpGenerator\Generator;
 
-require __DIR__ . '/vendor/autoload.php';
+require 'vendor/autoload.php';
 
 $app = new Application('wsdl2php', '2.1.0');
 $command = new GenerateCommand();
