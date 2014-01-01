58a953fd97b220e01dda9fd7778a0a944ee46335
Kasper Garnaes <kasper.garnaes@gmail.com>
2014-01-01 09:11:28 +0100

    Release 2.2.1

--- wsdl2php.php
+++ wsdl2php.php
@@ -8,7 +8,7 @@ use Wsdl2PhpGenerator\Generator;
 
 require 'vendor/autoload.php';
 
-$app = new Application('wsdl2php', '2.1.0');
+$app = new Application('wsdl2php', '2.2.1');
 $command = new GenerateCommand();
 $command->setGenerator(Generator::getInstance());
 $app->add($command);
