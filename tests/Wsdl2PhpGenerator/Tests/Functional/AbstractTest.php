3feee37baf7d319c5108c4c2c2a856db3312f77b
Kasper Garnaes <kasper.garnaes@gmail.com>
2014-02-12 09:34:39 +0100

    Update abstract test case.
    Use helper assertions for subclassing and method parameters.

--- tests/Wsdl2PhpGenerator/Tests/Functional/AbstractTest.php
+++ tests/Wsdl2PhpGenerator/Tests/Functional/AbstractTest.php
@@ -14,6 +14,8 @@ class AbstractTest extends Wsdl2PhpGeneratorFunctionalTestCase {
 
     public function testAbstract()
     {
+        $this->markTestIncomplete('Handling of abstract types is not implemented yet.');
+
         // The base service class should be available. This also loads all other generated classes.
         $this->assertGeneratedClassExists('AbstractServiceService');
 
@@ -26,8 +28,6 @@ class AbstractTest extends Wsdl2PhpGeneratorFunctionalTestCase {
         $this->assertNotContains('echo', $methods, 'Class should not contain a method called echo. It is a reserved keyword');
         $this->assertContains('aEcho', $methods, 'Class should contain a method with a derived name for echo since it is a reserved keyword');
 
-        $this->markTestIncomplete('Handling of abstract types and extensions is not implemented yet.');
-
         // The complex type Author is abstract in the WSDL and should thus also abstract when generated.
         $abstractClass = new \ReflectionClass('Author');
         $this->assertTrue($abstractClass->isAbstract(), 'A class representing a type with abstract="true" should be abstract');
@@ -35,21 +35,20 @@ class AbstractTest extends Wsdl2PhpGeneratorFunctionalTestCase {
         // Complex types UserAuthor and NonUserAuthor extends the User type. That relationship should be converted to
         // subclasses in the generated code.
         $subClassMessage = 'A class representing a type which extends another type should be a subclass of the corresponding class.';
-        $this->assertInstanceOf('Author', new \UserAuthor('foo'), $subClassMessage);
-        $this->assertInstanceOf('Author', new \NonUserAuthor('bar'), $subClassMessage);
+        $this->assertClassSubclassOf('UserAuthor', 'Author', $subClassMessage);
+        $this->assertClassSubclassOf('NonUserAuthor', 'Author', $subClassMessage);
         // Same goes for DerivedClass1, DerivedClass2 and BaseClass.
-        $this->assertInstanceOf('BaseClass', new \DerivedClass1('baz'), $subClassMessage);
-        $this->assertInstanceOf('BaseClass', new \DerivedClass2('boink'), $subClassMessage);
+        $this->assertClassSubclassOf('DerivedClass1', 'BaseClass', $subClassMessage);
+        $this->assertClassSubclassOf('DerivedClass2', 'BaseClass', $subClassMessage);
 
         // The constructor for subclasses should at least have the same parameters as the constructor of the class they
         // extend.
-        $subClassConstructorMessage = 'The constructor of a subclass should have all the parameters of the constructor of the parent class.';
         $baseClass = new \ReflectionClass('Author');
         $baseConstructor = $baseClass->getConstructor();
         $subClass = new \ReflectionClass('UserAuthor');
         $subClassConstructor = $subClass->getConstructor();
         foreach ($baseConstructor->getParameters() as $parameter) {
-            $this->assertContains($parameter, $subClassConstructor->getParameters(), $subClassConstructorMessage);
+            $this->assertMethodHasParameter($subClassConstructor, $parameter);
         }
     }
 }
