d753405c05b5153d07f1d6894d0c7e1b3ac349e1
Jim Schmid <jim.schmid@1up.io>
2014-04-16 07:38:45 +0200

    Added documentation blocks to the new interface resp. its implementing class.

--- src/Wsdl2PhpGenerator/ConfigInterface.php
+++ src/Wsdl2PhpGenerator/ConfigInterface.php
@@ -2,8 +2,33 @@
 
 namespace Wsdl2PhpGenerator;
 
+/**
+ * The config interface which implemented represents
+ * a configuration that is used across this project.
+ *
+ * @package Wsdl2PhpGenerator
+ * @author Jim Schmid <js@1up.com>
+ * @license http://www.opensource.org/licenses/mit-license.php MIT License
+ */
 interface ConfigInterface
 {
+    /**
+     * Get a value from the configuration
+     * by key.
+     *
+     * @param $key
+     * @return mixed
+     * @throws \InvalidArgumentException
+     */
     public function get($key);
+
+    /**
+     * Set or overwrite a configuration key with
+     * a given value.
+     *
+     * @param $key
+     * @param $value
+     * @return mixed
+     */
     public function set($key, $value);
 }
