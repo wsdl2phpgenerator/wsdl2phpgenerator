f26269c7fc28792b644e0a5158c7ad58b5c79a08
Sam Mousa <sam@mousa.nl>
2014-09-05 11:41:50 +0200

    Fixed PSR-1.

--- wsdl2php.php
+++ wsdl2php.php
@@ -6,13 +6,10 @@ use Wsdl2PhpGenerator\Console\Application;
 use Wsdl2PhpGenerator\Console\GenerateCommand;
 use Wsdl2PhpGenerator\Generator;
 
-if (file_exists('vendor/autoload.php'))
-{
-	require 'vendor/autoload.php';
-}
-else
-{
-	require __DIR__ . '/../../autoload.php';
+if (file_exists('vendor/autoload.php')) {
+    require 'vendor/autoload.php';
+} else {
+    require __DIR__ . '/../../autoload.php';
 }
 
 $app = new Application('wsdl2php', '2.4.2');
