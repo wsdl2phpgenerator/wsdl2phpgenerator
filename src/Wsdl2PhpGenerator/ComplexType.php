86d2f917745f3271face41285aeb31367dff264e
Kasper Garnaes <kasper.garnaes@gmail.com>
2014-11-03 21:45:54 +0100

    Refactor recursive base type member handling.
    Conflicts:
    	src/Wsdl2PhpGenerator/ComplexType.php

--- src/Wsdl2PhpGenerator/ComplexType.php
+++ src/Wsdl2PhpGenerator/ComplexType.php
@@ -49,33 +49,6 @@ class ComplexType extends Type
     }
 
     /**
-     * Generates constructor parameters from baseType tree
-     *
-     * @param ComplexType $baseType
-     * @param PhpDocComment $constructorComment
-     * @param string $constructorParameters
-     */
-    protected function generateConstructorFromBaseType(ComplexType $baseType, PhpDocComment $constructorComment, &$constructorParameters)
-    {
-        if ($baseType->baseType !== null) {
-            $this->generateConstructorFromBaseType($baseType->baseType, $constructorComment, $constructorParameters);
-        }
-        foreach ($baseType->getMembers() as $member) {
-            $type = Validator::validateType($member->getType());
-            $name = Validator::validateAttribute($member->getName());
-
-            if (!$member->getNillable()) {
-                $constructorComment->addParam(PhpDocElementFactory::getParam($type, $name, ''));
-                $constructorComment->setAccess(PhpDocElementFactory::getPublicAccess());
-                $constructorParameters .= ', $' . $name;
-                if ($this->config->getConstructorParamsDefaultToNull()) {
-                    $constructorParameters .= ' = null';
-                }
-            }
-        }
-    }
-
-    /**
      * Implements the loading of the class object
      *
      * @throws Exception if the class is already generated(not null)
@@ -104,8 +77,9 @@ class ComplexType extends Type
         $accessors = array();
 
         // Add base type members to constructor parameter list first and call base class constructor
-        if ($this->baseType !== null) {
-            foreach ($this->baseType->getMembers() as $member) {
+        $parentMembers = $this->getBaseTypeMembers($this);
+        if (!empty($parentMembers)) {
+            foreach ($parentMembers as $member) {
                 $type = Validator::validateType($member->getType());
                 $name = Validator::validateAttribute($member->getName());
 
@@ -202,5 +176,20 @@ class ComplexType extends Type
     public function getMembers()
     {
         return $this->members;
+    }
+
+    /**
+     * Get members from base types all the way through the type hierarchy.
+     *
+     * @param ComplexType $type The type to retrieve base type members from.
+     * @return Variable[] Member variables from all base types.
+     */
+    protected function getBaseTypeMembers(ComplexType $type)
+    {
+        $members = array();
+        if (!empty($type->baseType)) {
+            $members = array_merge($type->baseType->getMembers(), $this->getBaseTypeMembers($type->baseType));
+        }
+        return $members;
     }
 }
