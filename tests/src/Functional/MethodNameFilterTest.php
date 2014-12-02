7731ccff6a752a6c46ed4cb4a5506ddbf3251321
ivol84 <ivol84@gmail.com>
2014-12-02 20:38:08 +0300

    Restore and simplify book_shell.wsdl. Fix test.

--- tests/src/Functional/MethodNameFilterTest.php
+++ tests/src/Functional/MethodNameFilterTest.php
@@ -1,39 +1,35 @@
 <?php
 
-namespace Wsdl2PhpGenerator\Tests\Functional;
+    namespace Wsdl2PhpGenerator\Tests\Functional;
 
 
-class MethodNameFilterTest extends FunctionalTestCase
-{
-    public function testFilterByMethodName()
+    class MethodNameFilterTest extends FunctionalTestCase
     {
-        $this->assertGeneratedFileExists('AbstractServiceService.php');
-        $this->assertGeneratedFileExists('Author.php');
-        $this->assertGeneratedFileExists('Book.php');
-        $this->assertFileNotGenerated('BaseClass.php');
-        $this->assertFileNotGenerated('DerivedClass1.php');
-        $this->assertFileNotGenerated('DerivedClass2.php');
-        $this->assertFileNotGenerated('NicknameUserAuthor.php');
-        $this->assertFileNotGenerated('NonUserAuthor.php');
-        $this->assertFileNotGenerated('UserClass2.php');
-        $serviceClass = new \ReflectionClass('AbstractServiceService');
-        $methods = array_map(function (\ReflectionMethod $method) {
-            return $method->getName();
-        }, $serviceClass->getMethods());
-        $this->assertContains('aEcho', $methods);
-        $this->assertNotContains('echoLiteral', $methods);
-        $this->assertNotContains('echoDerived', $methods);
-    }
+        public function testFilterByMethodName()
+        {
+            $this->assertGeneratedFileExists('Method_Get_Book_Request.php');
+            $this->assertGeneratedFileExists('Method_Get_Book_Response.php');
+            $this->assertGeneratedFileExists('BookShell_Service.php');
+            $this->assertFileNotGenerated('Author_Response.php');
+            $this->assertFileNotGenerated('Method_Get_Authors_Request.php');
+            $this->assertFileNotGenerated('Method_Get_Authors_Response.php');
+            $serviceClass = new \ReflectionClass('BookShell_Service');
+            $methods = array_map(function (\ReflectionMethod $method) {
+                return $method->getName();
+            }, $serviceClass->getMethods());
+            $this->assertContains('Get_Book', $methods);
+            $this->assertNotContains('Get_Authors', $methods);
+        }
 
-    protected function configureOptions()
-    {
-        $this->config->set('methodNames', array('echo'));
+        protected function configureOptions()
+        {
+            $this->config->set('methodNames', array('Get_Book'));
+        }
+        /**
+         * @return string The path to the WSDL to generate code from.
+         */
+        protected function getWsdlPath()
+        {
+            return $this->fixtureDir . '/abstract/book_shell.wsdl';
+        }
     }
-    /**
-     * @return string The path to the WSDL to generate code from.
-     */
-    protected function getWsdlPath()
-    {
-        return $this->fixtureDir . '/abstract/abstract.wsdl';
-    }
-}
