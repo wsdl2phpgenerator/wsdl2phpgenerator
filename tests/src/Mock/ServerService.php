a2ae62fef50c8bf892764ea1a56523ff1e07db53
tbl0605 <tbl0605@gmail.com>
2015-08-11 19:07:05 +0200

    Generate a SOAPServer Class also #122

---
+++ tests/src/Mock/ServerService.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Wsdl2PhpGenerator\Tests\Mock;
+
+/**
+ * Mock \SoapServerTest\TestAbstractServerService to use when testing.
+ */
+class ServerService extends \SoapServerTest\TestAbstractServerService {
+}
