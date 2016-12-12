32b027f970c845ef64bfe9fe9b6499f7fb5526c4
Albin Kerouanton <albin.kerouanton@knplabs.com>
2016-12-12 11:17:46 +0100

    Fix bug produced by empty types

--- src/Filter/ServiceOperationFilter.php
+++ src/Filter/ServiceOperationFilter.php
@@ -47,6 +47,7 @@ class ServiceOperationFilter implements FilterInterface
             if (!$operation) {
                 continue;
             }
+
             // Discover types used in params
             foreach ($operation->getParams() as $param => $hint) {
                 $arr = $operation->getPhpDocParams($param, $service->getTypes());
@@ -57,7 +58,11 @@ class ServiceOperationFilter implements FilterInterface
             }
             // Discover types used in returns
             $returns = $operation->getReturns();
-            $methodTypes[] = $service->getType($returns);
+
+            $type = $service->getType($returns);
+            if ($type !== null) {
+                $methodTypes[] = $type;
+            }
 
             foreach ($methodTypes as $type) {
                 $methodTypes = array_merge($methodTypes, $this->findUsedTypes($service, $type)) ;
