3de6d0fbc75ee7ca3ed46d3a472ca0a0fa28a15e
Tuen Lee <leeyuentuen@gmail.com>
2018-07-02 14:06:00 +0200

    Update PhpClass.php
    count on a null value is not possible, it will throw an error, check the value before count

--- lib/PhpSource/PhpClass.php
+++ lib/PhpSource/PhpClass.php
@@ -153,7 +153,7 @@ class PhpClass extends PhpElement
             $ret .= ' extends ' . $this->extends;
         }
 
-        if (count($this->implements) > 0) {
+        if ($this->implements && count($this->implements) > 0) {
             $ret .= ' implements ' . implode(', ', $this->implements);
         }
 
