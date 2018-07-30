be152fb8f410f14fd24d0630ba7aca8f36253e60
Eric <ekeyte@rategenius.com>
2018-07-30 17:51:52 -0500

    Reverts the composer changes to see if we can get it working on widest range

--- tests/src/Unit/Wsdl2PhpGeneratorTestCase.php
+++ tests/src/Unit/Wsdl2PhpGeneratorTestCase.php
@@ -2,24 +2,9 @@
 
 namespace Wsdl2PhpGenerator\Tests\Unit;
 
-use RuntimeException;
-
-if (class_exists('\PHPUnit\Framework\TestCase')) {
-    /**
-     * Class Wsdl2PhpGeneratorTestCase
-     * PhpUnit 6+
-     */
-    class Wsdl2PhpGeneratorTestCase extends \PHPUnit\Framework\TestCase
-    {
-    }
-} elseif (class_exists('\PHPUnit_Framework_TestCase')) {
-    /**
-     * Class Wsdl2PhpGeneratorTestCase
-     * PhpUnit 5
-     */
-    class Wsdl2PhpGeneratorTestCase extends \PHPUnit_Framework_TestCase
-    {
-    }
-} else {
-    throw new RuntimeException('Unsupported PHPUnit version');
+/**
+ * Class Wsdl2PhpGeneratorTestCase
+ */
+class Wsdl2PhpGeneratorTestCase extends \PHPUnit_Framework_TestCase
+{
 }
