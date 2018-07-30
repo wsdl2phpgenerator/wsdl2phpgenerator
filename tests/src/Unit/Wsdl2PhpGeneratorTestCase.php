e8d1d31a86adc351e6d49cf812b98a27e1df8c35
Eric <ekeyte@rategenius.com>
2018-07-30 18:05:35 -0500

    Revert "Reverts the composer changes to see if we can get it working on widest range"
    This reverts commit be152fb8f410f14fd24d0630ba7aca8f36253e60.

--- tests/src/Unit/Wsdl2PhpGeneratorTestCase.php
+++ tests/src/Unit/Wsdl2PhpGeneratorTestCase.php
@@ -2,9 +2,24 @@
 
 namespace Wsdl2PhpGenerator\Tests\Unit;
 
-/**
- * Class Wsdl2PhpGeneratorTestCase
- */
-class Wsdl2PhpGeneratorTestCase extends \PHPUnit_Framework_TestCase
-{
+use RuntimeException;
+
+if (class_exists('\PHPUnit\Framework\TestCase')) {
+    /**
+     * Class Wsdl2PhpGeneratorTestCase
+     * PhpUnit 6+
+     */
+    class Wsdl2PhpGeneratorTestCase extends \PHPUnit\Framework\TestCase
+    {
+    }
+} elseif (class_exists('\PHPUnit_Framework_TestCase')) {
+    /**
+     * Class Wsdl2PhpGeneratorTestCase
+     * PhpUnit 5
+     */
+    class Wsdl2PhpGeneratorTestCase extends \PHPUnit_Framework_TestCase
+    {
+    }
+} else {
+    throw new RuntimeException('Unsupported PHPUnit version');
 }
