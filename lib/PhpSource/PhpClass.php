243e36eb564bf98ee503d25aea73099f3a3f6624
Varin6 <varinator@gmail.com>
2019-02-23 18:59:37 +0000

    Initialising $this->implements as an empty array to stop count() yielding a warning if NULL

--- lib/PhpSource/PhpClass.php
+++ lib/PhpSource/PhpClass.php
@@ -103,6 +103,7 @@ class PhpClass extends PhpElement
     public function __construct($identifier, $classExists = false, $extends = '', PhpDocComment $comment = null, $final = false, $abstract = false)
     {
         $this->dependencies = array();
+        $this->implements = array();
         $this->classExists = $classExists;
         $this->comment = $comment;
         $this->final = $final;
