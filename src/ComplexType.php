44408ee8902ce0ca5c9977eca868b4b0c442966a
Tuen Lee <yuen-tuen.lee@destiny.be>
2019-11-26 10:31:43 +0100

    Change class modifier to allowed serialize the whole class

--- src/ComplexType.php
+++ src/ComplexType.php
@@ -104,7 +104,7 @@ class ComplexType extends Type
 
             $comment = new PhpDocComment();
             $comment->setVar(PhpDocElementFactory::getVar($type, $name, ''));
-            $var = new PhpVariable('protected', $name, 'null', $comment);
+            $var = new PhpVariable('public', $name, 'null', $comment);
             $this->class->addVariable($var);
 
             if (!$member->getNullable()) {
